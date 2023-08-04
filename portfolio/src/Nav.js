56cc085867828f8db9b755311c8435eed330fd20
Simon Conrad <simondbconrad@gmail.com>
2023-08-04 13:58:00 -0400

    remove navbar toggle bootstrap funtionality

--- portfolio/src/Nav.js
+++ portfolio/src/Nav.js
@@ -6,18 +6,15 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function NavBar(){
     return(
-    <Navbar expand="lg" className="bg navbar">
+    <Navbar expand="lg" className="bg navbar fixed-top">
         <Container className="navbar-container">
             <Navbar.Brand className="brand" href="#home">Simon Conrad</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
                 <Nav.Link href="#about">About</Nav.Link>
                 <Nav.Link href="#projects">Projects</Nav.Link>
                 <Nav.Link href="#resume">Resume</Nav.Link>
                 <Nav.Link href="#contact">Contact</Nav.Link>
             </Nav>
-            </Navbar.Collapse>
         </Container>
     </Navbar>
   );
