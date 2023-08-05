1865779f1001f2c369a3b4b3881d11f70210da5e
Simon Conrad <simondbconrad@gmail.com>
2023-08-05 10:56:05 -0400

    working fix for flexbox bug

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './Nav'
+import About from './About'
 
 function App() {
   return (
     <BrowserRouter>
       <NavBar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={ <About /> } />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- portfolio/src/Nav.js
+++ portfolio/src/Nav.js
@@ -6,18 +6,20 @@ import Navbar from 'react-bootstrap/Navbar';
 
 function NavBar(){
     return(
-    <Navbar expand="lg" className="bg navbar fixed-top">
-        <Container className="navbar-container">
-            <Navbar.Brand className="brand" href="#home">Simon Conrad</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link href="#about">About</Nav.Link>
-                <Nav.Link href="#projects">Projects</Nav.Link>
-                <Nav.Link href="#resume">Resume</Nav.Link>
-                <Nav.Link href="#contact">Contact</Nav.Link>
-            </Nav>
-        </Container>
-    </Navbar>
-  );
+        <div className="navbar-container">
+            <Navbar expand="lg" className="bg navbar fixed-top">
+                <Container className="navbar-container">
+                    <Navbar.Brand className="brand" href="#home">Simon Conrad</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="#about">About</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#resume">Resume</Nav.Link>
+                        <Nav.Link href="#contact">Contact</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+        </div>
+          );
 }
 
 
