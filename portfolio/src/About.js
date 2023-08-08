6ec8de9e4a9e5660d93c027880200165b702b67c
Simon Conrad <simondbconrad@gmail.com>
2023-08-08 10:57:00 -0400

    format about grid

--- portfolio/src/About.js
+++ portfolio/src/About.js
@@ -3,9 +3,9 @@ import React from 'react'
 function About(){
     return(
         <div className="about-container">
-            <h1 className="about-content">Simon Conrad</h1>
-            <h5 className="about-content">Full Stack Developer | Web Designer</h5>
-            <p className="about-content">I am a hands-on worker, excited to find creative solutions in software and web development. I am determined to stay on top of new technologies as they surface and evolve. I learn best by putting programming concepts into practice in experimental projects and coding solutions. I tinker with my code, testing and experimenting frequently, to learn the structure of a framework and the under-the-hood functionality of a coding language through the development process. I am persistent in finding solutions to difficult problems and bug fixes. I take the time to research and tap all my available resources to pass a sticking point in development. During this process, I stay open to alternatives and opportunities to refactor in pursuit of clean, effective solutions. </p>
+            <h1 className="about-content name">Simon Conrad</h1>
+            <h5 className="about-content profession">Full Stack Developer | Web Designer</h5>
+            <p className="about-content bio">I am a hands-on worker, excited to find creative solutions in software and web development. I am determined to stay on top of new technologies as they surface and evolve. I learn best by putting programming concepts into practice in experimental projects and coding solutions. I tinker with my code, testing and experimenting frequently, to learn the structure of a framework and the under-the-hood functionality of a coding language through the development process. I am persistent in finding solutions to difficult problems and bug fixes. I take the time to research and tap all my available resources to pass a sticking point in development. During this process, I stay open to alternatives and opportunities to refactor in pursuit of clean, effective solutions. </p>
         </div>
     )
 }
