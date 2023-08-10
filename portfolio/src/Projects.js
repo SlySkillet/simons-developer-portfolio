41e418587561fcb1762a573fa100221fe0616e07
Simon Conrad <simondbconrad@gmail.com>
2023-08-10 14:40:20 -0400

    set project timeline container

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Projects(){
     return(
         <div className="projects-container">
-            <p>project-timeline</p>
+            <h2 className="project-title">project-timeline</h2>
         </div>
     )
 }
