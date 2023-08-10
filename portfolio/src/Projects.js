2a220d0e765a3928a90589609af4b861ffdfb919
Simon Conrad <simondbconrad@gmail.com>
2023-08-10 16:22:17 -0400

    create container to hold projects

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -4,6 +4,10 @@ function Projects(){
     return(
         <div className="projects-container">
             <h2 className="project-title">Projects</h2>
+            <div className="projects-timeline">
+                <div className="projects-container left"></div>
+                <div className="projects-container right"></div>
+            </div>
         </div>
     )
 }
