a8fdf9ccb7667cb45a9af34272f04d8901cb3ac5
Simon Conrad <simondbconrad@gmail.com>
2023-08-10 23:54:18 -0400

    add 3rd project

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -13,6 +13,9 @@ function Projects(){
                 <div className="project-content">
                     <p>This was a challenging project I took on in the second module of Hack Reactor. Many thanks to my partner Mac Stephens! Together we put together three microservices to handle an auto-dealership's inventory, services and sales. After completing a functional application, I did some experimenting with the user interface and gained some valuable experience incorporating bootstrap and custom css in React.</p>
                 </div>
+                <div className="project-content">
+                    <p>This is the first application I built from scratch using Django. It is a task manager for a contributors to a project to organize and visualize team progress. This was my first try at implementing a 3rd party api (plotly) and data visualization. I built a full stack application from scratch giving a project manager the ability to assign and monitor progress through a clean user interface with Django 4 framework. I inncorporated gantt charts in the user interface to provide visual representation of project tasks, their due dates and progress towards completion by integrating Plotly API.</p>
+                </div>
             </div>
         </div>
     )
