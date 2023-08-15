e3325c4d34360fb2ed1840227995c0175e98c2fe
Simon Conrad <simondbconrad@gmail.com>
2023-08-15 16:46:34 -0400

    add a spacer for visualization on left projects container

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -21,10 +21,12 @@ function Projects(){
                 </div>
             </div>
             <div className="project-container right">
+                <div className="spacer spacer-1"></div>
                 <div className="project-content">
                     <h5 className="project-description title">RadCar Automobiles</h5>
                     <p>This was a challenging project I took on in the second module of Hack Reactor. Many thanks to my partner Mac Stephens! Together we put together three microservices to handle an auto-dealership's inventory, services and sales. After completing a functional application, I did some experimenting with the user interface and gained some valuable experience incorporating bootstrap and custom css in React.</p>
                 </div>
+                <div className="spacer spacer-2"></div>
                 <div className="project-content">
                     <h5 className="project-description title">Task Manager</h5>
                     <p>This is the first application I built from scratch using Django. It is a task manager for a contributors to a project to organize and visualize team progress. This was my first try at implementing a 3rd party api (plotly) and data visualization. I built a full stack application from scratch giving a project manager the ability to assign and monitor progress through a clean user interface with Django 4 framework. I inncorporated gantt charts in the user interface to provide visual representation of project tasks, their due dates and progress towards completion by integrating Plotly API.</p>
