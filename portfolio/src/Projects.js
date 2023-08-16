02b8e0fd3108466292b6550f7f1b3d254ad5cf22
Simon Conrad <simondbconrad@gmail.com>
2023-08-16 15:07:43 -0400

    add tech stack section to projects

--- portfolio/src/Projects.js
+++ portfolio/src/Projects.js
@@ -18,10 +18,12 @@ function Projects(){
                         <p className="shaded-text links"> ~ Deployed app: <Link className="project-link" to="https://luckythirteen.gitlab.io/plunge/">Plunge</Link> | Repo: <Link className="project-link" to="https://gitlab.com/luckythirteen/plunge">GitLab</Link> ~ </p>
                     </div>
                     <p>I created this peer-to-peer marketplace application from scratch in a team of four cohort-mates at Hack Reactor. We began with the idea to build a platform housing informal classes for local people to share their knowledge, acquire new skills and foster community. This was my first experience taking a full stack application from seed to deployment. Over 6 weeks we wire-framed our vision for the application, built a development environment with docker, created our backend using Fast API and a SQL database, and created a dynamic user interface with React.js. This was a lesson in iterative development and agile methodology. Working in a team like this was a hands on lesson in Git workflow and general project organization. We used Linear to create and assign tickets and linked it up with Git Lab where we filled our merge requests. I am particularly proud of my work in implementing the Google Maps API and Redux toolkit into this project. The map centers on the authenticated user's address provided in their account information and accessed through the Redux store. Learning to work with global state and Google API were my highest priority learning goals in this project and I'm very happy with our product.</p>
+                    <div className="tech-stack-container">
+                        <p className="tech-stack">tech stack</p>
+                    </div>
                 </div>
             </div>
             <div className="project-container right">
-                <div className="spacer spacer-1"></div>
                 <div className="project-content">
                     <h5 className="project-description title">RadCar Automobiles</h5>
                     <div className="screenshots-container">
@@ -33,7 +35,6 @@ function Projects(){
                     </div>
                     <p>This was a challenging project I took on in the second module of Hack Reactor. Many thanks to my partner Mac Stephens! Together we put together three microservices to handle an auto-dealership's inventory, services and sales. After completing a functional application, I did some experimenting with the user interface and gained some valuable experience incorporating bootstrap and custom css in React.</p>
                 </div>
-                <div className="spacer spacer-2"></div>
                 <div className="project-content">
                     <h5 className="project-description title">Task Manager</h5>
                     <div className="screenshots-container">
