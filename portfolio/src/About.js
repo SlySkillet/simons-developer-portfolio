6503aa37890b66c4f6a1420900603d76cd81f5d2
Simon Conrad <simondbconrad@gmail.com>
2023-08-23 15:29:46 -0400

    formatting adjustments

--- portfolio/src/About.js
+++ portfolio/src/About.js
@@ -7,9 +7,11 @@ function About(){
                 <h1 className="about-content name">Simon</h1>
                 <h1 className="about-content name">Conrad</h1>
             </div>
-            <h5 className="about-content profession">Full Stack Developer | Web Designer</h5>
-            <img alt="simon" src={require('./images/simon-cartoon.JPEG')} height="300px"/>
-            <p className="sub-text">Python | Javascript | React | Django | FastAPI | SQL | Docker</p>
+            <div className="about-content-sub-container">
+                <h5 className="about-content profession">Full Stack Developer | Web Designer</h5>
+                <p className="sub-text">Python | Javascript | React | Django | FastAPI | SQL | Docker</p>
+            </div>
+            <img alt="simon" src={require('./images/simon-cartoon.JPEG')}/>
             <p className="about-content bio">I am a hands-on, creative developer with a background in art and fine dining. My interest in software began as a desire to create, solving puzzles along the way. Code brings together mathematical logic and artful design with endless and ever-evolving possibilities.</p>
             <p className="about-content bio two">Away from the computer, somewhere in the world, you’ll find me doing any combination of running, diving in deep water, snowboarding or making a new place home. Away from the computer, somewhere in the world, you’ll find me doing any combination of running, diving in deep water, snowboarding or making a new place home. </p>
             {/* <p className="sub-text">Hack Reactor Graduate - 2023 | BA Earlham College - 2014</p> */}
