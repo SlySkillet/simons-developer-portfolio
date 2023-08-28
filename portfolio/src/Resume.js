7ac2713a64ef8c8153d11030497e4b9e01489fb5
Simon Conrad <simondbconrad@gmail.com>
2023-08-28 16:25:52 -0400

    clean up

--- portfolio/src/Resume.js
+++ portfolio/src/Resume.js
@@ -7,7 +7,6 @@ function Resume (){
             <h2 className="project-title">Resume</h2>
             <div className="resume-container">
                 <iframe className="resume-img" title="resume" src={require('./images/simon_conrad_resume.pdf')}/>
-                {/* <img className="resume-img" alt="resume" src={require('./images/resume-screenshot.png')} /> */}
             </div>
         </div>
     )
