f4a8ddb37004266a41f15c6bbed44e1816f91e19
Simon Conrad <simondbconrad@gmail.com>
2023-08-29 13:01:45 -0400

    render title for contact page in container

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
 function Contact(){
-    return (<p>contact page</p>)
+    return(
+        <div className="projects-container">
+            <div className='about-content-name-container'>
+                <h2 className="project-title">Contact</h2><h2 className="project-title">Me</h2>
+            </div>
+
+        </div>
+    )
 }
 
 export default Contact
