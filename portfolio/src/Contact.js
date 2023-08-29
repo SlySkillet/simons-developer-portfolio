f81715733658339652e02ada1edc66a01617559f
Simon Conrad <simondbconrad@gmail.com>
2023-08-29 17:34:39 -0400

    finalize contact page - looking fresh

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import Button from 'react-bootstrap/Button';
+import {FaLinkedin
+, FaGithubAlt
+} from 'react-icons/fa'
 
 function Contact(){
     return(
@@ -9,10 +12,19 @@ function Contact(){
                 <h2 className="project-title">Contact</h2><h2 className="project-title">Me</h2>
             </div>
             <div className="resume-container">
-                <h3 className="contact-subtitle">Social:</h3>
-                <div className="contact-links-container">
-                    <Link to="https://www.linkedin.com/in/simon-conrad/"><Button variant="outline-info">LinkedIn</Button></Link>
-                    <Link to="https://github.com/SlySkillet"><Button variant="outline-info">GitHub</Button></Link>
+                <div className="project-content contact-container">
+                    <h3 className="contact-subtitle">Social:</h3>
+                    <div className="contact-links-container">
+                        <Link to="https://www.linkedin.com/in/simon-conrad/"><Button variant="outline-info">LinkedIn < FaLinkedin /></Button></Link>
+                        <Link to="https://github.com/SlySkillet"><Button variant="outline-info">GitHub < FaGithubAlt /></Button></Link>
+                    </div>
+                    <h3 className="contact-subtitle">Email:</h3>
+                    <Link className="contact-email" to="">simondbconrad@gmail.com</Link>
+                    <h3 className="contact-subtitle">Discord:</h3>
+                    <p className="contact-email">SlySkillet#1031</p>
+                    <div className="project-detail-container contact">
+                        <p className="contact-detail">I am active on all of the above platforms. Please connect with me on LinkedIn and reach out by whatever method you prefer. I am open to collaborate on projects and open for work. Looking forward to hearing from you. Cheers! </p>
+                    </div>
                 </div>
             </div>
 
