87f875c623e391053780a4797bb189d0b7063bc0
Simon Conrad <simondbconrad@gmail.com>
2023-08-29 16:31:23 -0400

    fix hover effects on buttons

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Button from 'react-bootstrap/Button';
 
 function Contact(){
     return(
@@ -6,6 +8,14 @@ function Contact(){
             <div className='about-content-name-container'>
                 <h2 className="project-title">Contact</h2><h2 className="project-title">Me</h2>
             </div>
+            <div className="resume-container">
+                <h3 className="contact-subtitle">Social:</h3>
+                <div className="contact-links-container">
+                    <Link to="https://www.linkedin.com/in/simon-conrad/"><Button variant="outline-info">LinkedIn</Button></Link>
+                    <Link to="https://github.com/SlySkillet"><Button variant="outline-info">GitHub</Button></Link>
+                </div>
+            </div>
+
 
         </div>
     )
