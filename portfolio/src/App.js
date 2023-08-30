37a95d455110e1af7c8d45efc39215165dc165ec
Simon Conrad <simondbconrad@gmail.com>
2023-08-30 14:21:05 -0400

    change path for main page

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <NavBar />
       <div className="main-container">
         <Routes>
-          <Route path="/" element={ <About /> } />
+          <Route path="/simons-developer-portfolio/" element={ <About /> } />
           <Route path="projects/" element={ <Projects />} />
           <Route path="resume/" element={ <Resume />} />
           <Route path="contact/" element={ <Contact /> }/>
