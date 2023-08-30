6ba2396acf68d60f1e326ee10077f0fbd99dee5f
Simon Conrad <simondbconrad@gmail.com>
2023-08-30 15:21:58 -0400

    another test

--- portfolio/src/Nav.js
+++ portfolio/src/Nav.js
@@ -11,7 +11,7 @@ function NavBar(){
                 <Container className="navbar-container">
                     <Navbar.Brand className="brand" href="#home">S.C.</Navbar.Brand>
                     <Nav className="me-auto">
-                        <Nav.Link href="/#/">About</Nav.Link>
+                        <Nav.Link href="simons-developer-portfolio/#/">About</Nav.Link>
                         <Nav.Link href="/#/projects">Projects</Nav.Link>
                         <Nav.Link href="/#/resume">Resume</Nav.Link>
                         {/* <Nav.Link href="#blog">Blog</Nav.Link> */}
