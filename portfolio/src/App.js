82f7dd8befddaa677727098e02b042a204af0969
Simon Conrad <simondbconrad@gmail.com>
2023-08-30 15:00:08 -0400

    replace browser router with hash router

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './Nav'
 import About from './About'
 import Footer from './Footer'
@@ -10,18 +10,18 @@ import Contact from './Contact'
 function App() {
   return (
 <div className="grid-container">
-    <BrowserRouter>
+    <HashRouter basename='/simons-developer-portfolio'>
       <NavBar />
       <div className="main-container">
         <Routes>
-          <Route path="#about" element={ <About /> } />
+          <Route path="/" element={ <About /> } />
           <Route path="projects/" element={ <Projects />} />
           <Route path="resume/" element={ <Resume />} />
           <Route path="contact/" element={ <Contact /> }/>
         </Routes>
       </div>
       {/* <Footer /> */}
-    </BrowserRouter>
+    </HashRouter>
 </div>
   );
 }
