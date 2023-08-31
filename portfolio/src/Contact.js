467af0f94b914b5d8e050f6bd76da7341f480545
Simon Conrad <simondbconrad@gmail.com>
2023-08-31 17:08:54 -0400

    touch up contact page - change email to p tag

--- portfolio/src/Contact.js
+++ portfolio/src/Contact.js
@@ -19,7 +19,7 @@ function Contact(){
                         <Link to="https://github.com/SlySkillet"><Button variant="outline-info">GitHub < FaGithubAlt /></Button></Link>
                     </div>
                     <h3 className="contact-subtitle">Email:</h3>
-                    <Link className="contact-email" to="">simondbconrad@gmail.com</Link>
+                    <p className="contact-email" to="">simondbconrad@gmail.com</p>
                     <h3 className="contact-subtitle">Discord:</h3>
                     <p className="contact-email">SlySkillet#1031</p>
                     <div className="project-detail-container contact">
