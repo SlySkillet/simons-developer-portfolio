24c49449231d57abaf529cee2c60fc759261764b
Simon Conrad <simondbconrad@gmail.com>
2023-09-01 15:16:36 -0400

    create parameters for ProjectCard

---
+++ portfolio/src/projectCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ProjectCard({
+    projectTitle,
+    completedDate,
+    imageUrls,
+    techStack,
+    description,
+    liveLink,
+    repoLink
+}) {
+    return(<p>returning</p>)
+}
+
+export default ProjectCard
