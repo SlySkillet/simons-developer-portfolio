c9d0fa974fa4d5eecc5ab6dcc959e42a5029fe58
Simon Conrad <simondbconrad@gmail.com>
2023-10-19 10:04:02 -0400

    fix footer links

--- src/Footer.js
+++ src/Footer.js
@@ -9,8 +9,8 @@ function Footer(){
             <p className="thanks">Thanks for visiting!</p>
             <Link className="nav-link contact" to="/contact" >Contact Me</Link >
             <div className='footer-link'>
-                <Link className="nav-link" to="/contact"><SiLinkedin /></Link >
-                <Link className="nav-link" to="/contact"><SiGithub /></Link >
+                <Link className="nav-link" to="https://www.linkedin.com/in/simon-conrad/" target="_blank" rel="noopener noreferrer"><SiLinkedin /></Link >
+                <Link className="nav-link" to="https://github.com/SlySkillet" target="_blank" rel="noopener noreferrer"><SiGithub /></Link >
             </div>
 
         </div>
