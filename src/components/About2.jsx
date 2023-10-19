13d2e6a0b15df3624e9e3c509e7ce5f3869cba4b
Simon Conrad <simondbconrad@gmail.com>
2023-10-19 17:05:56 -0400

    final edits for v2
    exit()
    q
    clear

--- src/components/About2.jsx
+++ src/components/About2.jsx
@@ -2,22 +2,26 @@ function About2() {
   return (
     <div id="about2" className="about2-container">
       <div className="about2-container-inner">
-        <h3 className="about2-text">Hack Reactor Graduate - 2023</h3>
+        <h4 className="about2-text">Hack Reactor Graduate - 2023</h4>
         <p className="about2-text">
-          I finished this 19-week immersive software engineering program in
-          August of 2023. This program revolved around project-based learning
-          using popular web development frameworks including Django, FastAPI,
-          and React. I learned programming fundamentals and studied computer
-          science algorithms in Python and Javascript.
+          I finished this 19-week software engineering immersive in August of
+          2023. This program revolved around project-based learning using
+          popular web development frameworks including Django, FastAPI, and
+          React. I learned programming fundamentals and studied computer science
+          algorithms in Python and Javascript.
         </p>
-        <h3 className="about2-text">Goals:</h3>
+        <h4 className="about2-text">My Goals:</h4>
         <p className="about2-text">
-          At the moment, I am seeking a role as a full stack engineer. With so
-          many skills to practice, my intention is to hone my understanding of
-          technology across the stack. I am particularly interested in
-          contributing to early stage startups at this phase of my career. My
-          dream is for my work to contribute to a safe, clean, and prosperous
-          future.{" "}
+          At the moment, I am seeking a role as a full stack engineer to hone my
+          understanding of technology across the stack. I intend to explore
+          quantitative data and data visualization to create impactful software
+          for mission-driven companies. I believe our future depends on us
+          harnessing pragmatic, innovative technology.{" "}
+        </p>
+        <p className="about2-text">
+          In this phase of my career, I am particularly interested in
+          contributing to an early stage startup. My dream is for my work to
+          contribute to a safe, healthy, and prosperous future.
         </p>
       </div>
     </div>
