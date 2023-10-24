4713218e91d8259c6cd1bfbb534078251794a8bc
Simon Conrad <simondbconrad@gmail.com>
2023-10-24 14:04:38 -0400

    create right side carrot box

--- src/Blog.js
+++ src/Blog.js
@@ -24,7 +24,18 @@ function Blog() {
           </div>
         </div>
         <div className="blog-col2">
-          <div className="right blog-post"></div>
+          <div className="blog-spacer-1"></div>
+          <div className="right blog-post">
+            <p className="blog-content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
         </div>
       </div>
     </div>
