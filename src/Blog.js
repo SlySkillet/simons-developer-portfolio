6ed7b8661e74013c1671b1fd59cc0c3dfb632ddd
Simon Conrad <simondbconrad@gmail.com>
2023-10-24 13:44:16 -0400

    create carrot box for left entry

--- src/Blog.js
+++ src/Blog.js
@@ -10,8 +10,22 @@ function Blog() {
         </p>
       </div>
       <div className="main-blog">
-        <div className="blog-col1"></div>
-        <div className="blog-col2"></div>
+        <div className="blog-col1">
+          <div className="left blog-post">
+            <p className="blog-content">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
+        <div className="blog-col2">
+          <div className="right blog-post"></div>
+        </div>
       </div>
     </div>
   );
