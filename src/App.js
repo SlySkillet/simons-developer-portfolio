6cac837f7debc3f2c11475bd022534467e38c60c
Simon Conrad <simondbconrad@gmail.com>
2023-10-25 16:06:39 -0400

    route up a new Blog page for a fresh slate

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from "./Nav";
 import Resume from "./Resume";
 import Contact from "./Contact";
 import Home from "./Home";
-import Blog from "./Blog";
+import Blog2 from "./Blog2";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="resume/" element={<Resume />} />
             <Route path="contact/" element={<Contact />} />
-            <Route path="blog/" element={<Blog />} />
+            <Route path="blog/" element={<Blog2 />} />
           </Routes>
         </div>
       </BrowserRouter>
---
+++ src/Blog2.js
@@ -0,0 +1,16 @@
+import "./Blog2.css";
+
+function Blog2() {
+  return (
+    <div className="blog-container">
+      <div className="blog header">
+        <p className="blog title">:: Blog ::</p>
+        <p className="blog subtext">
+          come here for your daily dose of the goods
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Blog2;
