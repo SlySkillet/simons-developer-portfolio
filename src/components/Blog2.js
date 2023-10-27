332d94de81f125e3d312677dc258e0f94b48bf12
Simon Conrad <simondbconrad@gmail.com>
2023-10-27 11:08:27 -0400

    create initial post template

--- src/components/Blog2.js
+++ src/components/Blog2.js
@@ -1,6 +1,7 @@
 import "./Blog2.css";
 import { FaEarlybirds } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import Post1 from "./blog-posts/post1";
 
 function Blog2() {
   return (
@@ -13,34 +14,7 @@ function Blog2() {
       </div>
       <div className="main-blog">
         <div className="timeline">
-          <div className="blog-post-container left">
-            <div className="timeline-icon left-icon">
-              <div className="icon-wrapper">
-                <i className="icon">
-                  <FaEarlybirds />
-                </i>
-              </div>
-            </div>
-            <div className="blog-post">
-              <h4 className="post-title">header</h4>
-              <p className="post-content">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-              <ui className="post-content">
-                <li>
-                  something to say and a <Link>link</Link>
-                </li>
-                <li>something else</li>
-                <li>some other stuff</li>
-              </ui>
-            </div>
-          </div>
+          <Post1 />
           <div className="blog-post-container notfirst right">
             <div className="timeline-icon right-icon">
               <div className="icon-wrapper">
---
+++ src/components/blog-posts/post1.jsx
@@ -0,0 +1,50 @@
+import "../Blog2.css";
+import { FaEarlybirds } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { GiCyberEye } from "react-icons/gi";
+
+function Post1() {
+  return (
+    <div className="blog-post-container left">
+      <div className="timeline-icon left-icon">
+        <div className="icon-wrapper">
+          <i className="icon">
+            <FaEarlybirds />
+          </i>
+        </div>
+      </div>
+      <div className="blog-post">
+        <p className="post-date"> Oct 20, 2023</p>
+        <h4 className="post-title">
+          Blogging - it shows you give a shit about something
+        </h4>
+        <div className="inner-post-container">
+          <GiCyberEye className="thumbnail post1" />
+          <p className="post-content">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum. quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+            esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+            cupidatat non proident, sunt in culpa qui officia deserunt mollit
+            anim id est laborum.
+          </p>
+          <ui className="post-content">
+            <li>
+              something to say and a <Link>link</Link>
+            </li>
+            <li>something else</li>
+            <li>some other stuff</li>
+          </ui>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Post1;
